767d6cd87e32dc4064b68dd192c078f50a708aac
Bruno Dias <bruno.dias@vtex.com.br>
2018-12-20 15:06:53 -0300

    Fix tests

--- react/__test__/Menu.test.js
+++ react/__test__/Menu.test.js
@@ -9,7 +9,7 @@ describe('Menu Component', () => {
   let wrapper
 
   beforeEach(() => {
-    const messages = require('../locales/en-US.json')
+    const messages = require('../../messages/en-US.json')
     wrapper = render(
       <IntlProvider locale="en-US" messages={messages}>
         <Menu />
