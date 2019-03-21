0052f5da8548cb2f9fc8582b78d0bceacd82ee00
Breno Calazans <brenocalazans@gmail.com>
2019-03-21 18:39:21 -0300

    Update to @vtex/test-tools

--- react/MenuLink.test.js
+++ react/MenuLink.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { render } from 'test-utils'
+import { render } from '@vtex/test-tools/react'
 
-import Menu from '../index'
+import Menu from './index'
 
 describe('Menu Component', () => {
   function renderComponent() {
