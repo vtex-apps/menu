cd1d4e7594783eff0290cb3765255008e205bcf1
Rerisson Matos <rerisson.matos@splab.ufcg.edu.br>
2019-04-04 20:31:12 -0300

    Use categoryId to decide when to render children

--- react/components/CategoryItem.tsx
+++ react/components/CategoryItem.tsx
@@ -2,7 +2,14 @@ import React, { Component } from 'react'
 import { Query } from 'react-apollo'
 
 import category from '../graphql/category.graphql'
+<<<<<<< HEAD
 import StyledLink, { StyledLinkProps } from './StyledLink'
+=======
+import MenuItem from '../MenuItem'
+import SubMenu from '../Submenu'
+import StyledLink from './StyledLink'
+import Menu from '../Menu';
+>>>>>>> Use categoryId to decide when to render children
 
 class CategoryItem extends Component<CategoryItemProps, State> {
   public static getDerivedStateFromError() {
@@ -16,7 +23,18 @@ class CategoryItem extends Component<CategoryItemProps, State> {
   }
 
   public render() {
+<<<<<<< HEAD
     const { categoryId, text, ...rest } = this.props
+=======
+    const {
+      categoryId,
+      text,
+      typography,
+      isHovered,
+      highlight,
+      showChildrenCategory = 16,
+    } = this.props
+>>>>>>> Use categoryId to decide when to render children
 
     if (this.state.hasError) {
       return null
