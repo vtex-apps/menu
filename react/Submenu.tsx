3e34b92af0680d04f23edd5d724161c3ecaa2d54
Lucas Bebber <lucasbbebber@gmail.com>
2019-04-25 10:51:19 -0300

    Remove collapsible mode from regular submenu

--- react/Submenu.tsx
+++ react/Submenu.tsx
@@ -1,42 +1,27 @@
 import classNames from 'classnames'
 import React from 'react'
 import { defineMessages } from 'react-intl'
-import SubmenuCollapsible from './components/SubmenuCollapsible'
 
 const Submenu: StorefrontFunctionComponent<SubmenuProps> = ({
   isOpen,
   width,
-  mode,
   children,
-}) => {
-  const isCollapsible = mode === SubmenuMode.collapsible
-
-  if (isCollapsible) {
-    return (
-      <SubmenuCollapsible isOpen={isOpen}>
-        {children}
-      </SubmenuCollapsible>
-    )
-  }
-
-  return (
-    <div className={`${width === '100%' ? '' : 'relative'}`}>
-      <div
-        className={classNames(
-          isCollapsible ? 'pv4 pl2' : 'absolute left-0 bg-base pv4 bw1 bb b--muted-3 z-2',
-          {
-            dn: !isOpen,
-            flex: isOpen,
-            'w-100': width === '100%',
-            'w-auto ml6': width === 'auto',
-          }
-        )}
-      >
-        <section className="w-100 flex justify-center">{children}</section>
-      </div>
+}) => (
+  <div className={`${width === '100%' ? '' : 'relative'}`}>
+    <div
+      className={classNames('absolute left-0 bg-base pv4 bw1 bb b--muted-3 z-2',
+        {
+          dn: !isOpen,
+          flex: isOpen,
+          'w-100': width === '100%',
+          'w-auto ml6': width === 'auto',
+        }
+      )}
+    >
+      <section className="w-100 flex justify-center">{children}</section>
     </div>
-  )
-}
+  </div>
+)
 
 export enum SubmenuMode {
   regular = 'regular',
