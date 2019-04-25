c89a958b803472e7d004274eb1e74eded1d0b879
Lucas Bebber <lucasbbebber@gmail.com>
2019-04-25 10:51:01 -0300

    Remove SubmenuCollapsible from components

---
+++ react/SubmenuCollapsible.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { defineMessages } from 'react-intl'
+import Collapsible from './components/Collapsible'
+
+interface Props {
+  isOpen: boolean
+}
+
+const SubmenuCollapsible: StorefrontFunctionComponent<Props> = ({
+  isOpen,
+  children,
+}) => (
+  <Collapsible open={isOpen}>
+    <div className="pl4 flex">
+      <section
+        className="w-100 flex"
+        style={{
+          WebkitOverflowScrolling: 'touch',
+          maxHeight: 400,
+          overflowY: 'scroll',
+        }}>
+        <div>
+          {children}
+        </div>
+      </section>
+    </div>
+  </Collapsible>
+)
+
+const messages = defineMessages({
+  submenuTitle: {
+    defaultMessage: '',
+    id: 'editor.menu.submenu.title',
+  },
+})
+
+SubmenuCollapsible.getSchema = () => ({
+  title: messages.submenuTitle.id,
+  type: 'object',
+})
+
+export default SubmenuCollapsible
