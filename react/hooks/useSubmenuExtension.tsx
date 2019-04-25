e17d4b7c7129c1acb094708ea791b82525cdf572
Lucas Bebber <lucasbbebber@gmail.com>
2019-04-25 10:55:13 -0300

    Create temporary useSubmenuExtension hook

---
+++ react/hooks/useSubmenuExtension.tsx
@@ -0,0 +1,25 @@
+import { useRuntime, useTreePath } from 'vtex.render-runtime'
+
+/** This is a throwaway function, while useChildBlock doesn't return
+ * the "implements" block info, or which specialization of the block
+ * is being used (i.e. whether its `submenu` or `submenu.collapsible`)
+ */
+const useSubmenuExtension = () => {
+  const runtime = useRuntime()
+  const treePathContext = useTreePath()
+
+  const { treePath } = treePathContext
+  const { extensions } = runtime
+
+  if (!extensions || !treePath) {
+    return {}
+  }
+
+  const submenuTreePath = `${treePath}/submenu`
+
+  const extension = extensions[submenuTreePath]
+
+  return extension || {}
+}
+
+export default useSubmenuExtension
