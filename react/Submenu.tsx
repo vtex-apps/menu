8c13a5eeb5b14f87ded09b12148723f1dcca6844
Lucas Bebber <lucasbbebber@gmail.com>
2019-06-03 13:51:14 -0300

    Add support for vertical orientation and padding top/bottom on Submenu

--- react/Submenu.tsx
+++ react/Submenu.tsx
@@ -1,15 +1,45 @@
 import classNames from 'classnames'
+import { range } from 'ramda'
 import React from 'react'
 import { defineMessages } from 'react-intl'
+
+const MAX_TACHYONS_SCALE = 11
+export type TachyonsScaleInput = string | number | undefined
+
+/** TODO: This function is copied from the flex-layout app.
+ * It should probably be exported to a separate package.
+ */
+const parseTachyonsValue = (value: TachyonsScaleInput, name?: string) => {
+  if (!value) {
+    return 0
+  }
+
+  const supportedValues = range(0, MAX_TACHYONS_SCALE + 1).map(String)
+
+  if (!supportedValues.includes(String(value))) {
+    if (name) {
+      console.warn(
+        `Invalid ${name} value ("${value}"). It should be an integer between 0 and ${MAX_TACHYONS_SCALE}.`
+      )
+    }
+
+    return 0
+  }
+
+  return typeof value === 'string' ? parseInt(value, 10) : value
+}
 
 const Submenu: StorefrontFunctionComponent<SubmenuProps> = ({
   isOpen,
   width,
   children,
+  orientation = Orientation.horizontal,
+  paddingTop = 4,
+  paddingBottom = 4,
 }) => (
   <div className={`${width === '100%' ? '' : 'relative'}`}>
     <div
-      className={classNames('absolute left-0 bg-base pv4 bw1 bb b--muted-3 z-2',
+      className={classNames(`absolute left-0 bg-base pt${parseTachyonsValue(paddingTop)} pb${parseTachyonsValue(paddingBottom)} bw1 bb b--muted-3 z-2`,
         {
           dn: !isOpen,
           flex: isOpen,
@@ -18,13 +48,22 @@ const Submenu: StorefrontFunctionComponent<SubmenuProps> = ({
         }
       )}
     >
-      <section className="w-100 flex justify-center">{children}</section>
+      <section className={classNames('w-100 flex justify-center', { 'flex-column': orientation === Orientation.vertical })}>{children}</section>
     </div>
   </div>
 )
 
+enum Orientation {
+  horizontal = 'horizontal',
+  vertical = 'vertical',
+}
+
+
 export interface SubmenuProps extends SubmenuSchema {
   isOpen: boolean
+  orientation: Orientation
+  paddingTop: number | string
+  paddingBottom: number | string
 }
 
 interface SubmenuSchema {
