a65631189294ce2ce48b676b6408c9727dbd8f40
Lucas Bebber <lucasbbebber@gmail.com>
2019-07-15 17:54:09 -0300

    Expand the parent of Collapsible menus when its children are expanded

--- react/SubmenuAccordion.tsx
+++ react/SubmenuAccordion.tsx
@@ -1,36 +1,34 @@
 import classNames from 'classnames'
 import React from 'react'
 import { defineMessages } from 'react-intl'
-import Collapsible from './components/Collapsible'
+import Collapsible, { CollapsibleContextConsumer } from './components/Collapsible'
 
 import { generateBlockClass } from '@vtex/css-handles'
 
 import styles from './SubmenuAccordion.css'
 
 interface Props {
-  isOpen: boolean,
+  isOpen: boolean
   blockClass?: string
 }
 
 const SubmenuAccordion: StorefrontFunctionComponent<Props> = ({
   isOpen,
   children,
-  blockClass
+  blockClass,
 }) => {
   const classes = generateBlockClass(styles.submenuAccordion, blockClass)
 
   return (
-    <Collapsible open={isOpen}>
-      <section
-        className={classNames(classes, "w-100 flex pl4 flex")}
-        style={{
-          WebkitOverflowScrolling: 'touch',
-          maxHeight: 400,
-          overflowY: 'scroll',
-        }}>
-        {children}
-      </section>
-    </Collapsible>
+    <CollapsibleContextConsumer>
+      {({ updateParentHeight }) => (
+        <Collapsible open={isOpen} updateParentHeight={updateParentHeight}>
+          <section className={classNames(classes, 'w-100 flex pl4 flex')}>
+            {children}
+          </section>
+        </Collapsible>
+      )}
+    </CollapsibleContextConsumer>
   )
 }
 
--- react/components/Collapsible.tsx
+++ react/components/Collapsible.tsx
@@ -1,20 +1,24 @@
-import React from 'react'
+import React, { FunctionComponent, Ref, RefForwardingComponent, PropsWithChildren } from 'react'
+
+const TRANSITION_DELAY = 200
 
 interface Props {
   open: boolean
-  transition: number
+  updateParentHeight?: (offset?: number) => void
 }
 
 interface State {
   height: number | string
 }
 
-class Collapsible extends React.Component<Props, State> { 
-  /*tslint:disable member-ordering */
-  public static defaultProps = {
-    transition: 200,
-  }
+interface CollapsibleContextValue {
+  updateParentHeight?: (offset?: number) => void
+}
 
+const CollapsibleContext = React.createContext<CollapsibleContextValue>({})
+
+class Collapsible extends React.Component<Props, State> {
+  /*tslint:disable member-ordering */
   private container = React.createRef<HTMLDivElement>()
 
   public state = {
@@ -31,46 +35,100 @@ class Collapsible extends React.Component<Props, State> {
 
   public componentDidUpdate(prevProps: Props) {
     if (!prevProps.open && this.props.open) {
-      const element = this.container.current
-      if (!element) {
-        return
+      if (typeof this.props.updateParentHeight === 'function') {
+        const childrenHeight = this.updateHeight()
+        this.props.updateParentHeight(childrenHeight)
+      } else {
+        this.updateHeight()
       }
-
-      element.style.height = 'auto'
-      const childrenHeight = element.offsetHeight
-
-      element.style.height = '0'
-
-      /** Forces layout in order to make the transition
-       * to the new height work.
-       */
-      this.forceLayout(element)
-
-      this.setState({
-        height: childrenHeight,
-      })
     } else if (prevProps.open && !this.props.open) {
-      this.setState({ height: 0 })
+      const childrenHeight = this.updateHeight()
+      if (typeof this.props.updateParentHeight === 'function') {
+        this.props.updateParentHeight(-childrenHeight)
+      }
+    }
+  }
+
+  public updateHeight = (offset?: number) => {
+    const element = this.container.current
+    if (!element) {
+      return 0
+    }
+
+    const initialHeight = element.offsetHeight
+
+    element.style.height = 'auto'
+    const childrenHeight = element.offsetHeight
+
+    element.style.height = `${initialHeight}px`
+
+    /** Forces layout in order to make the transition
+     * to the new height work.
+     */
+    this.forceLayout(element)
+
+    const targetHeight = Math.max(
+      0,
+      (this.props.open ? childrenHeight : 0) + (offset || 0)
+    )
+
+    this.setState({ height: targetHeight })
+
+    return childrenHeight
+  }
+
+  public updateHeightFromChildren = (offset?: number) => {
+    this.updateHeight(offset)
+  }
+
+  private handleUpdateParentHeight = (updateParentHeight: CollapsibleContextValue['updateParentHeight']) => (offset?: number) => {
+    this.updateHeightFromChildren(offset)
+    if (typeof updateParentHeight === 'function') {
+      updateParentHeight(offset)
     }
   }
 
   public render() {
-    const { children, transition } = this.props
+    const { children } = this.props
     const { height } = this.state
 
     return (
-      <div
-        className="overflow-hidden"
-        ref={this.container}
-        style={{
-          height,
-          transition: `height ${transition}ms ease-out`,
-        }}>
-          {children}
-      </div>
+      <CollapsibleContext.Consumer>
+        {({ updateParentHeight }) => (
+          <CollapsibleContext.Provider
+            value={{ updateParentHeight: this.handleUpdateParentHeight(updateParentHeight) }}
+          >
+            <CollapsibleContainer height={height} ref={this.container}>
+              {children}
+            </CollapsibleContainer>
+          </CollapsibleContext.Provider>
+        )}
+      </CollapsibleContext.Consumer>
     )
   }
 }
 
+type CollapsibleContainerProps = PropsWithChildren<{
+  height: number 
+}>
+
+const CollapsibleContainer = React.forwardRef(
+  (({ children, height }, ref) => (
+    <div
+      className="overflow-hidden"
+      ref={ref}
+      style={{
+        height,
+        transition: `height ${TRANSITION_DELAY}ms ease-out`,
+      }}
+    >
+      {children}
+    </div>
+  )) as RefForwardingComponent<HTMLDivElement, CollapsibleContainerProps>
+) 
+
 export default Collapsible
 
+const CollapsibleContextConsumer = CollapsibleContext.Consumer
+
+export { CollapsibleContextConsumer }
