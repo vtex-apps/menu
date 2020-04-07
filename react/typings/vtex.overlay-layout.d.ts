0ead63ead6843070306c45d0bfd91f399bfce386
Klynger <rafael.klynger@gmail.com>
2020-04-07 15:38:58 -0300

    Use setup of typings

--- react/typings/vtex.overlay-layout.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'vtex.overlay-layout' {
-  const BaseTrigger: React.FC<any>
-  const OverlayLayout: React.FC<any>
-  const OverlayTrigger: React.FC<any>
-  const BaseOverlay: React.FC<any>
-}
