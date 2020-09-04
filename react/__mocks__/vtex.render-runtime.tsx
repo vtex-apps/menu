a971322f059038b7d19737017efab02efa8a254c
Lucas Bebber <lucasbbebber@gmail.com>
2020-09-03 22:46:09 -0300

    Add useRuntime mock

--- react/__mocks__/vtex.render-runtime.tsx
+++ react/__mocks__/vtex.render-runtime.tsx
@@ -9,3 +9,7 @@ interface Props {
  */
 // eslint-disable-next-line jsx-a11y/anchor-is-valid
 export const Link: React.FC<Props> = props => <a href="#">{props.children}</a>
+
+export const useRuntime = () => ({
+  getSettings: () => ({}),
+})
