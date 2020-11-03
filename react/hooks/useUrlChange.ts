9b080046dcc684d91cb215d727edd98c4348bdbb
Christian Kaisermann <christian@kaisermann.me>
2020-11-03 12:04:28 -0300

    Memoize useUrlChange callback

--- react/hooks/useUrlChange.ts
+++ react/hooks/useUrlChange.ts
@@ -1,13 +1,14 @@
-import { useEffect } from 'react'
+import { useCallback, useEffect } from 'react'
 import { useRuntime } from 'vtex.render-runtime'
 import type { Listener } from 'history'
 
 export const useUrlChange = (fn: Listener, deps: unknown[]) => {
   const { history } = useRuntime()
 
+  const memoizedCallback = useCallback(fn, deps)
+
   useEffect(() => {
     // cancel method is returned to be executed whenever our deps change
-    return history?.listen(fn)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [fn, history, ...deps])
+    return history?.listen(memoizedCallback)
+  }, [memoizedCallback, history])
 }
