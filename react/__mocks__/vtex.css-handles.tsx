dbb7679ee37cb0bc8988fc84228de16039e45fe5
victorhmp <victor2142@gmail.com>
2020-12-09 14:54:03 -0300

    Fix vtex.css-handles mock

--- react/__mocks__/vtex.css-handles.tsx
+++ react/__mocks__/vtex.css-handles.tsx
@@ -1,8 +1,11 @@
-export const useCssHandles = (cssHandles: string[]) => {
+export const useCssHandles = (
+  cssHandles: string[],
+  options?: Record<string, any>
+) => {
   const handles: any = {}
   cssHandles.forEach(handle => {
     handles[handle] = handle
   })
 
-  return handles
+  return { handles }
 }
